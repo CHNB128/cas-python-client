1c6a0933b696a04213cebb3cc01d9a256ac25d33
drake.aren <drake.aren@protonmail.com>
2020-11-21 13:07:51 +0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='cas-client',
+   version='1.0',
+   description='Python client for CAS service',
+   author='Drake Aren Nelson',
+   author_email='drake.aren@pm.me',
+   packages=['cas-client'],  #same as name
+)
