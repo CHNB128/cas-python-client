4ffb9443a7ec3f76bc5c6c57d994ec5a61cf7dc5
Drake Nelson <drake.aren@protonmail.com>
2020-11-21 13:09:55 +0600

    Fix package name

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-   name='cas-client',
+   name='cas_client',
    version='1.0',
    description='Python client for CAS service',
    author='Drake Aren Nelson',
    author_email='drake.aren@pm.me',
-   packages=['cas-client'],
+   packages=['cas_client'],
 )
